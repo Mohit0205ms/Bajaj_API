aa62fce6b8adb512ede6260be78bc1d9d10bf1f1
Mohit0205ms <106728295+Mohit0205ms@users.noreply.github.com>
2024-03-18 06:18:47 +0000

    first comment

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/bfhl', (req, res) => {
 });
 
 
-const PORT = "https://bajaj-api-nu.vercel.app/";
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
